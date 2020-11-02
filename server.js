af0cbd905e6c33fad187824fe70f69d7f26bf4b4
liam-mack <liammma9@gmail.com>
2020-11-02 14:41:29 -0500

    removed __dirname from path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
